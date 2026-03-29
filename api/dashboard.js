98c8614bebd07d8b5a4b992c3b364c04f7c3a2d7
jrncprs-create <jrncprs@gmail.com>
2026-03-29 12:06:24 +0200

    Update file path for clara.json in API handler

--- api/dashboard.js
+++ api/dashboard.js
@@ -3,12 +3,11 @@ import path from "path";
 
 export default function handler(req, res) {
   try {
-    const filePath = path.join(process.cwd(), "data", "clara.json");
+    const filePath = path.join(process.cwd(), "api", "data", "clara.json");
     const fileData = fs.readFileSync(filePath, "utf8");
     const data = JSON.parse(fileData);
 
     res.status(200).json(data);
-
   } catch (e) {
     res.status(500).json({
       error: "failed_to_read_data",
